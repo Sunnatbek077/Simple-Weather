4013c24d7d76a6ad6cf7a9b728db7434d6a35956
Sunnatbek077 <sunnatbekabdunabiyev@icloud.com>
2025-08-31 17:38:45 +0500

    Initial Commit

---
+++ Simple-Weather/Simple_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_WeatherApp.swift
+//  Simple-Weather
+//
+//  Created by Sunnatbek on 31/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Simple_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
